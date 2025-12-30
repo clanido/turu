5ad2e2803d84096a2a421cc675b3509c5e6f12b2
Rayyan Akhtar Setiawan <rayyanaktarsetiawan@gmail.com>
2025-12-30 16:31:15 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      spacing: {
+        13: '3.25rem',
+      },
+      fontFamily: {
+        bogle: ['"DM Sans"', 'sans-serif'],
+      },
+      animation: {
+        'spin-slow': 'spin 3s linear infinite',
+        'goyang': 'goyang 1s ease-in-out infinite',
+      },
+      keyframes: {
+        goyang: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        },
+      },
+    },
+  },
+  plugins: [],
+}
